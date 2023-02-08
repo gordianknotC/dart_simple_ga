1e9a6a40f9a13b633b3d019a089d812bc22cb70b
curtis <curtis@fillygaming888.com>
2023-02-08 14:22:21 +0800

    Fix null safety

--- example/ga_simple_example.dart
+++ example/ga_simple_example.dart
@@ -1,11 +1,13 @@
 import 'package:ga_simple/ga_simple.dart';
 
 final documentDirectory = '';
-GoogleAnalytics _ga;
+GoogleAnalytics? _ga;
 GoogleAnalytics get ga{
   return _ga ??= GoogleAnalytics(
-      trackingId: 'your_id_here', analyticsUrl: 'http://your.url.net',
-      applicationName: 'your_app_name', applicationVersion: '1.0.0',
+      trackingId: 'your_id_here',
+      analyticsUrl: 'http://your.url.net',
+      applicationName: 'your_app_name',
+      applicationVersion: '1.0.0',
       documentDirectory: documentDirectory
   );
 }
--- lib/mobile/ga_mobile.dart
+++ lib/mobile/ga_mobile.dart
@@ -5,11 +5,14 @@ import '../sketches/ga_sketch.dart';
 
 
 class GoogleAnalytics implements AnalyticsSketch{
-	AnalyticsIO _delegate;
+	late AnalyticsIO _delegate;
 	
 	GoogleAnalytics({
-		String trackingId, String applicationName, String applicationVersion,
-		String analyticsUrl, String documentDirectory
+		required String trackingId,
+    required String applicationName,
+    required String applicationVersion,
+    required String analyticsUrl,
+    required String documentDirectory
 	}){
 		_delegate = AnalyticsIO(
 			trackingId, applicationName, applicationVersion,
@@ -19,22 +22,22 @@ class GoogleAnalytics implements AnalyticsSketch{
 	}
 	
   @override
-  void sendEvent(String category, String action, {String label, Map<String,String> parameters, int value}) {
+  void sendEvent(String category, String action, {String? label, Map<String,String>? parameters, int? value}) {
 		_delegate.sendEvent(category, action, label: label, parameters: parameters, value: value);
   }
 
   @override
-  void sendException(String description, {bool fatal}) {
+  void sendException(String description, {bool? fatal}) {
     _delegate.sendException(description, fatal: fatal);
   }
 
   @override
-  void sendPageView({String viewName, Map<String,String> parameters}) {
+  void sendPageView({String? viewName, Map<String,String>? parameters}) {
 		_delegate.sendScreenView(viewName, parameters: parameters);
   }
 
   @override
-  void sendTiming(String category, String variable, int valueMillis, {String label}) {
+  void sendTiming(String category, String variable, int valueMillis, {String? label}) {
     _delegate.sendTiming(variable, valueMillis, label: label, category: category);
   }
 	
